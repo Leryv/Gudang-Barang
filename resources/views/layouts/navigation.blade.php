9840f28d33ed1ecea8d15db58c8c54894a03d358
Leryv <andikagamers50@gmail.com>
2024-03-23 13:56:56 +0700

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,112 +6,83 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo
-                            class="block h-10 w-auto fill-current text-gray-600"
-                        ></x-application-logo>
+                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600"></x-application-logo>
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link
-                        :href="route('dashboard')"
-                        :active="request()->routeIs('dashboard')"
-                    >
-                        {{ __("Dashboard") }}
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
                     </x-nav-link>
                     @can('create permintaan stock')
-                    <x-nav-link
-                        :href="route('barang.index')"
-                        :active="request()->routeIs('view.stock-barang')"
-                    >
-                        {{ __("ReStock Barang") }}
-                    </x-nav-link>
-                    @endcan @can('create stock barang')
-                    <x-nav-link
-                        :href="route('barang.index')"
-                        :active="request()->routeIs('barang.index')"
-                    >
-                        {{ __("Master Barang") }}
-                    </x-nav-link>
+                        <x-nav-link :href="route('barang.index')" :active="request()->routeIs('view.stock-barang')">
+                            {{ __('ReStock Barang') }}
+                        </x-nav-link>
+                        @endcan @can('create stock barang')
+                        <x-nav-link :href="route('barang.index')" :active="request()->routeIs('barang.index')">
+                            {{ __('Master Barang') }}
+                        </x-nav-link>
 
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button
-                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                >
-                                    <div>{{ __("Master Data") }}</div>
+                        <div class="hidden sm:flex sm:items-center sm:ml-6">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                        <div>{{ __('Master Data') }}</div>
 
-                                    <div class="ml-1">
-                                        <svg
-                                            class="fill-current h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20"
-                                        >
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"
-                                            />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
+                                        <div class="ml-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
 
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('brand.index')">
-                                    {{ __("Brand") }}
-                                </x-dropdown-link>
-                                <x-dropdown-link
-                                    :href="route('kategori.index')"
-                                >
-                                    {{ __("Kategori") }}
-                                </x-dropdown-link>
-                                <x-dropdown-link :href="route('satuan.index')">
-                                    {{ __("Uom") }}
-                                </x-dropdown-link>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button
-                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                >
-                                    <div>{{ __("Transaksi") }}</div>
+                                <x-slot name="content">
+                                    <x-dropdown-link :href="route('brand.index')">
+                                        {{ __('Brand') }}
+                                    </x-dropdown-link>
+                                    <x-dropdown-link :href="route('kategori.index')">
+                                        {{ __('Kategori') }}
+                                    </x-dropdown-link>
+                                    <x-dropdown-link :href="route('satuan.index')">
+                                        {{ __('Uom') }}
+                                    </x-dropdown-link>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+                        <div class="hidden sm:flex sm:items-center sm:ml-6">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                        <div>{{ __('Transaksi') }}</div>
 
-                                    <div class="ml-1">
-                                        <svg
-                                            class="fill-current h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20"
-                                        >
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"
-                                            />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
+                                        <div class="ml-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
 
-                            <x-slot name="content">
-                                <x-dropdown-link
-                                    :href="route('transaksi.masuk')"
-                                >
-                                    {{ __("Masuk") }}
-                                </x-dropdown-link>
-                                <x-dropdown-link
-                                    :href="route('transaksi.keluar')"
-                                >
-                                    {{ __("Keluar") }}
-                                </x-dropdown-link>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
+                                <x-slot name="content">
+                                    <x-dropdown-link :href="route('transaksi.masuk')">
+                                        {{ __('Request') }}
+                                    </x-dropdown-link>
+                                    <x-dropdown-link :href="route('transaksi.keluar')">
+                                        {{ __('Request Approved') }}
+                                    </x-dropdown-link>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
                     @endcan
                 </div>
             </div>
@@ -121,21 +92,15 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
-                            class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
-                        >
+                            class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
-                                <svg
-                                    class="fill-current h-4 w-4"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path
-                                        fill-rule="evenodd"
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd"
-                                    />
+                                        clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </button>
@@ -146,12 +111,10 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link
-                                :href="route('logout')"
+                            <x-dropdown-link :href="route('logout')"
                                 onclick="event.preventDefault();
-                                                this.closest('form').submit();"
-                            >
-                                {{ __("Log Out") }}
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -160,32 +123,14 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button
-                    @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                >
-                    <svg
-                        class="h-6 w-6"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            :class="{'hidden': open, 'inline-flex': ! open }"
-                            class="inline-flex"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16"
-                        />
-                        <path
-                            :class="{'hidden': ! open, 'inline-flex': open }"
-                            class="hidden"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12"
-                        />
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -193,13 +138,10 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link
-                :href="route('dashboard')"
-                :active="request()->routeIs('dashboard')"
-            >
-                {{ __("Dashboard") }}
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
 
@@ -219,12 +161,10 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link
-                        :href="route('logout')"
+                    <x-responsive-nav-link :href="route('logout')"
                         onclick="event.preventDefault();
-                                        this.closest('form').submit();"
-                    >
-                        {{ __("Log Out") }}
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
--- resources/views/transaksi/keluar.blade.php
+++ resources/views/transaksi/keluar.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl leading-tight">
-            {{ __("Transaksi Keluar") }}
+            {{ __('Transaksi Keluar') }}
         </h2>
     </x-slot>
 
@@ -9,17 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <h5 class="text-gray-500 m-5">
                 <a href="">Transaksi</a> /
-                <a href="{{ route('transaksi.masuk') }}">Barang Masuk</a>
+                <a href="{{ route('transaksi.masuk') }}">Request Barang</a>
             </h5>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="flex justify-center">
                     <div class="min-w-full grid gap-4 items-center p-6">
-                        <table
-                            class="w-full text-sm text-left text-gray-500 shadow-md"
-                        >
-                            <thead
-                                class="text-xs text-gray-700 uppercase bg-gray-50"
-                            >
+                        <table class="w-full text-sm text-left text-gray-500 shadow-md">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
                                         Referensi
@@ -46,44 +42,39 @@
                             </thead>
                             <tbody>
                                 @forelse ($transaksis as $transaksi)
-                                <tr class="bg-white border-b">
-                                    <th
-                                        scope="row"
-                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                                    >
-                                        {{ $transaksi->kode_transaksi}}
-                                    </th>
-                                    <td class="px-6 py-4 text-gray-600">
-                                        {{ $transaksi->barang->brand->brand}}
-                                    </td>
-                                    <td class="px-6 py-4 text-gray-600">
-                                        {{ $transaksi->jumlah_permintaan}}
-                                    </td>
-                                    <td class="px-6 py-4 text-gray-600">
-                                        {{ $transaksi->created_at->format('d-m-y')}}
-                                    </td>
-                                    <td class="px-6 py-4 text-gray-600">
-                                        {{$transaksi->user->name}}
-                                    </td>
-                                    <td class="px-6 py-4 text-gray-600">
-                                        Rp. {{$transaksi->total_harga}}
-                                    </td>
-                                    <td
-                                        class="px-6 py-4 text-green-500 font-bold capitalize"
-                                    >
-                                        <div
-                                            class="w-max flex justify-center items-center bg-white shadow-lg rounded-lg text-sm px-2 py-2 mr-2 mb-2"
-                                        >
-                                            {{ $transaksi->status}}
-                                        </div>
-                                    </td>
-                                </tr>
+                                    <tr class="bg-white border-b">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                            {{ $transaksi->kode_transaksi }}
+                                        </th>
+                                        <td class="px-6 py-4 text-gray-600">
+                                            {{ $transaksi->barang->brand->brand }}
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-600">
+                                            {{ $transaksi->jumlah_permintaan }}
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-600">
+                                            {{ $transaksi->created_at->format('d-m-y') }}
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-600">
+                                            {{ $transaksi->user->name }}
+                                        </td>
+                                        <td class="px-6 py-4 text-gray-600">
+                                            Rp. {{ $transaksi->total_harga }}
+                                        </td>
+                                        <td class="px-6 py-4 text-green-500 font-bold capitalize">
+                                            <div
+                                                class="w-max flex justify-center items-center bg-white shadow-lg rounded-lg text-sm px-2 py-2 mr-2 mb-2">
+                                                {{ $transaksi->status }}
+                                            </div>
+                                        </td>
+                                    </tr>
                                 @empty
-                                <tr class="bg-white border-b">
-                                    <td class="px-6 py-4 text-gray-600">
-                                        Transaksi Tidak Tersedia
-                                    </td>
-                                </tr>
+                                    <tr class="bg-white border-b">
+                                        <td class="px-6 py-4 text-gray-600">
+                                            Transaksi Tidak Tersedia
+                                        </td>
+                                    </tr>
                                 @endforelse
                             </tbody>
                         </table>
