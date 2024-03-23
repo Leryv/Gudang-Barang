2d457dbf1cb9ea2cb850c67719f4240207b32ae0
Leryv <andikagamers50@gmail.com>
2024-03-23 13:57:57 +0700

    up

--- resources/views/transaksi/keluar.blade.php
+++ resources/views/transaksi/keluar.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl leading-tight">
-            {{ __('Transaksi Keluar') }}
+            {{ __('Request Approved') }}
         </h2>
     </x-slot>
 
